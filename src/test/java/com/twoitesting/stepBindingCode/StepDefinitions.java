448ba2fb1b28e99954218dcd07823139719149b1
Maciej Fec <Maciej.Fec@2itesting.com>
2022-06-28 16:50:38 +0100

    started work on step definitions

--- src/test/java/com/twoitesting/stepBindingCode/StepDefinitions.java
+++ src/test/java/com/twoitesting/stepBindingCode/StepDefinitions.java
@@ -1,32 +1,65 @@
 package com.twoitesting.stepBindingCode;
 
 import com.twoitesting.baseClasses.BaseClass;
+import com.twoitesting.pomPages.LoginPOM;
+import com.twoitesting.pomPages.MyAccountPOM;
+import com.twoitesting.pomPages.NavbarPOM;
+import com.twoitesting.utilityClasses.LoginDetailsReader;
+import io.cucumber.java.After;
 import io.cucumber.java.Before;
+import io.cucumber.java.en.And;
 import io.cucumber.java.en.Given;
 import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
 
-import org.junit.jupiter.api.Assertions.*;
+import java.io.FileNotFoundException;
 
 public class StepDefinitions extends BaseClass {
 
+    private LoginPOM loginPage;
+    private NavbarPOM navbar;
+    private MyAccountPOM myAccountPage;
     @Before
     public void setUp() {
 
         super.setUp();
         super.getDriver().get(super.getBaseUrl());
+
+        this.loginPage = new LoginPOM(super.getDriver());
+        this.navbar = new NavbarPOM(super.getDriver());
+        this.myAccountPage = new MyAccountPOM(super.getDriver());
     }
 
+    @After
     public void tearDown() {
 
+        // If not on my account page, navigate to my account page.
+        String currentUrl = super.getDriver().getCurrentUrl();
+
+        if(!currentUrl.contains("my-account")) {
+            this.navbar.navigateToMyAccount();
+        }
+
+        this.myAccountPage.clickLogoutBtn();
         super.tearDown();
     }
 
     @Given("I am logged in")
     public void i_am_logged_in() {
-        // Write code here that turns the phrase above into concrete actions
-        throw new io.cucumber.java.PendingException();
+
+        LoginDetailsReader loginDetailsReader = new LoginDetailsReader();
+        String username = loginDetailsReader.getUsername();
+        String password = loginDetailsReader.getPassword();
+
+        this.loginPage.login(username, password);
     }
+
+    @Given("I am on the shop page")
+    public void i_am_on_the_shop_page() {
+
+        this.navbar.navigateToShopPage();
+    }
+
     @Given("I add an item to the cart")
     public void i_add_an_item_to_the_cart() {
         // Write code here that turns the phrase above into concrete actions
