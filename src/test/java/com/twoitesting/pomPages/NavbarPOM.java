88a442a2da75c69e067dfdc47847c8769edf8081
Maciej Fec <Maciej.Fec@2itesting.com>
2022-06-28 16:49:57 +0100

    created page object model for the navigation bar

--- src/test/java/com/twoitesting/pomPages/NavbarPOM.java
+++ src/test/java/com/twoitesting/pomPages/NavbarPOM.java
@@ -1,4 +1,33 @@
 package com.twoitesting.pomPages;
 
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+
 public class NavbarPOM {
+
+    private WebDriver driver;
+
+    public NavbarPOM(WebDriver driver) {
+
+        this.driver = driver;
+        PageFactory.initElements(this.driver, this);
+    }
+
+    @FindBy(linkText = "My account")
+    private WebElement myAccountTab;
+
+    @FindBy(linkText = "Shop")
+    private WebElement shopTab;
+
+    public void navigateToMyAccount() {
+
+        this.myAccountTab.click();
+    }
+
+    public void navigateToShopPage() {
+
+        this.shopTab.click();
+    }
 }
