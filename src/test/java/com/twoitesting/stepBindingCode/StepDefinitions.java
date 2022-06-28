ef2dd05991dbbcec6f4f349b8c83aff34696f8c9
Maciej Fec <Maciej.Fec@2itesting.com>
2022-06-28 14:42:28 +0100

    added step definitions skeleton

--- src/test/java/com/twoitesting/stepBindingCode/StepDefinitions.java
+++ src/test/java/com/twoitesting/stepBindingCode/StepDefinitions.java
@@ -8,4 +8,34 @@ import org.junit.jupiter.api.Assertions.*;
 
 public class StepDefinitions {
 
+    @Given("I am logged in")
+    public void i_am_logged_in() {
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
+    }
+    @Given("I add an item to the cart")
+    public void i_add_an_item_to_the_cart() {
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
+    }
+    @Given("I am on the cart page")
+    public void i_am_on_the_cart_page() {
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
+    }
+    @Given("I input discount code {string}")
+    public void i_input_discount_code(String string) {
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
+    }
+    @When("I apply the discount code")
+    public void i_apply_the_discount_code() {
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
+    }
+    @Then("The total is correct after discount and shipping is applied")
+    public void the_total_is_correct_after_discount_and_shipping_is_applied() {
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
+    }
 }
