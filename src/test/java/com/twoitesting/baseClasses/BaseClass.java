450bdcdf63d966733dd9235ff31f1c5a97192dd6
Maciej Fec <Maciej.Fec@2itesting.com>
2022-06-28 16:10:12 +0100

    added temporaty implicit wait

--- src/test/java/com/twoitesting/baseClasses/BaseClass.java
+++ src/test/java/com/twoitesting/baseClasses/BaseClass.java
@@ -6,6 +6,8 @@ import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.edge.EdgeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
 
+import java.time.Duration;
+
 public class BaseClass {
 
     private WebDriver driver;
@@ -43,6 +45,7 @@ public class BaseClass {
                 break;
         }
 
+        this.driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(20));
         this.driver.manage().window().maximize();
     }
 
