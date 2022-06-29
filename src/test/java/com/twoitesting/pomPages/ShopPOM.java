b82ea1896cd5cacf0b4d10833bb081cf7b774d84
Maciej Fec <Maciej.Fec@2itesting.com>
2022-06-29 14:44:18 +0100

    created page object model for the shop page

--- src/test/java/com/twoitesting/pomPages/ShopPOM.java
+++ src/test/java/com/twoitesting/pomPages/ShopPOM.java
@@ -1,4 +1,35 @@
 package com.twoitesting.pomPages;
 
+import org.openqa.selenium.Keys;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+
 public class ShopPOM {
+
+    private WebDriver driver;
+
+    public ShopPOM(WebDriver driver) {
+
+        this.driver = driver;
+        PageFactory.initElements(this.driver, this);
+    }
+
+    @FindBy(name = "s")
+    private WebElement searchBar;
+
+    @FindBy(css = "#main > header")
+    private WebElement header;
+
+    public void searchForItem(String item) {
+
+        this.searchBar.clear();
+        this.searchBar.sendKeys(item + Keys.ENTER);
+    }
+
+    public String getHeaderText() {
+
+        return this.header.getText();
+    }
 }
