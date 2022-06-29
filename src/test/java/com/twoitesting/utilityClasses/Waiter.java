8906ffd19ad56ec966f76816242efe4a4e57da14
Maciej Fec <Maciej.Fec@2itesting.com>
2022-06-29 17:07:56 +0100

    added a custom wait function

--- src/test/java/com/twoitesting/utilityClasses/Waiter.java
+++ src/test/java/com/twoitesting/utilityClasses/Waiter.java
@@ -1,4 +1,17 @@
 package com.twoitesting.utilityClasses;
 
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import java.time.Duration;
+
 public class Waiter {
+
+    public static void waitForElementToBeClickable(WebDriver driver, int seconds, By locator) {
+
+        WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(seconds));
+        wait.until(ExpectedConditions.elementToBeClickable(locator));
+    }
 }
