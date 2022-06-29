48ec49da75f63214ea0ed8a503cbc745045c49db
Maciej Fec <Maciej.Fec@2itesting.com>
2022-06-29 16:59:32 +0100

    created basic method to remove all elements from cart, needs updating

--- src/test/java/com/twoitesting/pomPages/CartPOM.java
+++ src/test/java/com/twoitesting/pomPages/CartPOM.java
@@ -1,4 +1,36 @@
 package com.twoitesting.pomPages;
 
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+
+import java.util.List;
+
 public class CartPOM {
+
+    private WebDriver driver;
+
+    public CartPOM(WebDriver driver) {
+
+        this.driver = driver;
+        PageFactory.initElements(this.driver, this);
+    }
+
+    @FindBy(css = "a[aria-label='Remove this item']")
+    private List<WebElement> itemsToBeRemoved;
+
+    public void emptyCart() {
+
+        for(int i = this.itemsToBeRemoved.size() - 1; i >= 0; i--) {
+            try {
+                Thread.sleep(2000);
+            } catch (InterruptedException e) {
+                throw new RuntimeException(e);
+            }
+
+            this.itemsToBeRemoved.get(i).click();
+        }
+    }
 }
