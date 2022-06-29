3f3d87f561e51df012825a93a760918015d394ed
Maciej Fec <Maciej.Fec@2itesting.com>
2022-06-29 15:03:27 +0100

    created page object model for the product page

--- src/test/java/com/twoitesting/pomPages/ProductPOM.java
+++ src/test/java/com/twoitesting/pomPages/ProductPOM.java
@@ -1,4 +1,25 @@
 package com.twoitesting.pomPages;
 
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+
 public class ProductPOM {
+
+    private WebDriver driver;
+
+    public ProductPOM(WebDriver driver) {
+
+        this.driver = driver;
+        PageFactory.initElements(this.driver, this);
+    }
+
+    @FindBy(name = "add-to-cart")
+    private WebElement addToCartBtn;
+
+    public void addItemToCart() {
+
+        this.addToCartBtn.click();
+    }
 }
