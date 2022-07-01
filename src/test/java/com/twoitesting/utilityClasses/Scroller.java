8678bf27deab4dbf5af9272e4f280950d21e66f1
Maciej Fec <Maciej.Fec@2itesting.com>
2022-07-01 09:23:06 +0100

    added static method to scroll to an element

--- src/test/java/com/twoitesting/utilityClasses/Scroller.java
+++ src/test/java/com/twoitesting/utilityClasses/Scroller.java
@@ -1,4 +1,14 @@
 package com.twoitesting.utilityClasses;
 
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.interactions.Actions;
+
 public class Scroller {
+
+    public static void scrollToElement(WebDriver driver, WebElement element) {
+
+        Actions scrollAction = new Actions(driver);
+        scrollAction.scrollToElement(element);
+    }
 }
