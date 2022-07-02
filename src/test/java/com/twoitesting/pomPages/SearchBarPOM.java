1e935d0418ac0fb058dbd9f1e32d94b8125860b8
Maciej Fec <Maciej.Fec@2itesting.com>
2022-07-02 14:21:14 +0100

    added search bar element and search method

--- src/test/java/com/twoitesting/pomPages/SearchBarPOM.java
+++ src/test/java/com/twoitesting/pomPages/SearchBarPOM.java
@@ -1,4 +1,27 @@
 package com.twoitesting.pomPages;
 
+import org.openqa.selenium.Keys;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+
 public class SearchBarPOM {
+
+    private WebDriver driver;
+
+    public SearchBarPOM(WebDriver driver) {
+
+        this.driver = driver;
+        PageFactory.initElements(this.driver, this);
+    }
+
+    @FindBy(name = "s")
+    private WebElement searchBar;
+
+    public void searchForItem(String item) {
+
+        this.searchBar.clear();
+        this.searchBar.sendKeys(item + Keys.ENTER);
+    }
 }
