87b8f880b32f02ae2cf7e22cb40acb709d8a78fc
Maciej Fec <Maciej.Fec@2itesting.com>
2022-07-04 11:59:55 +0100

    created page object model

--- src/test/java/com/twoitesting/pomPages/OrdersPOM.java
+++ src/test/java/com/twoitesting/pomPages/OrdersPOM.java
@@ -1,4 +1,27 @@
 package com.twoitesting.pomPages;
 
+import com.twoitesting.utilityClasses.Waiter;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+
 public class OrdersPOM {
+
+    private WebDriver driver;
+
+    public OrdersPOM(WebDriver driver) {
+
+        this.driver = driver;
+        PageFactory.initElements(this.driver, this);
+    }
+
+    @FindBy(css = "tr > td")
+    private WebElement latestOrderNumTableData;
+
+    public String getLatestOrderNumber() {
+
+        Waiter.waitForElementToBeVisible(this.driver, 5, this.latestOrderNumTableData);
+        return this.latestOrderNumTableData.getText().replace("#", "");
+    }
 }
